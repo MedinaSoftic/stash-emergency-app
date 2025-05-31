1811882a0b88dc0d08cc0bf06d56c2d1777501b0
MedinaSoftic <108442934+MedinaSoftic@users.noreply.github.com>
2025-05-31 13:32:23 -0500

    Fixed minor bugs with nav bar, added a table of information

--- vite-stash-emergency-app/src/components/ContactPg/Contact.jsx
+++ vite-stash-emergency-app/src/components/ContactPg/Contact.jsx
@@ -1,13 +1,34 @@
 import React from "react"
-import { Link } from 'react-router'
 import ContactCard from "./ContactCard"
 import LinkButton from "../LinkButton"
 import "./Card.css"
+import {minnesota, wisconsin, missouri, kentucky} from "./Shelters"
+import {useTable} from "react-table"
+import "./ShelterTable.css"
 
 
 
 //Contact card logic pased in by props
 export default function Contact() {
+    const data = React.useMemo(() => (
+        [...minnesota, ...wisconsin, ...missouri, ...kentucky]), [])
+    const columns = React.useMemo(() => [
+      {
+        Header: "State",
+        accessor: "state",
+      },
+      {
+        Header: "Building Name",
+        accessor: "buildingName",
+      },
+      {
+        Header: "Address",
+        accessor: "address",
+      }      
+    ], []);
+
+    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data});
+
 
     return (
         <>
@@ -31,7 +52,6 @@ export default function Contact() {
                     <div className="card-inner">
                         <div className="front">
                             <h2>USA.Gov</h2>
-                            <p></p>
                         </div>
                         <div className="back">
                             <ContactCard title="USAGov" number="1-844-872-4681" email="AskIA@fema.dhs.gov" mail="FEMA P.O. Box 10055 Hyattsville, MD 20782-8055"></ContactCard>                
@@ -45,18 +65,62 @@ export default function Contact() {
                         <div className="card-inner">
                             <div className="front">
                                 <h2>Red Cross</h2>
-                                <p></p>
                             </div>
                             <div className="back">
                                 <ContactCard title="Red Cross" number="1-800-733-2767" email="customercare@redcross.org" mail="430 17th Street, NW, Washington, DC 20006"></ContactCard>
                                 <p>Red Cross can help with the reunification of Family in the US and US Territories, after a natural disaster. For guidence visit their <a href="https://www.redcross.org/get-help/disaster-relief-and-recovery-services/contact-and-locate-loved-ones.html">Contact Loved Ones.</a></p>
-                                <p></p>
+                                </div>
                             </div>
+                            </div>
+
+                            {/* Youtube video embeded */}
+                            <div className="containerFrame"> 
+                            <iframe className="responsiveFrame" width="560" height="315" src="https://www.youtube.com/embed/l3GJqNX5x9M?si=_OYDT6U1ObbHwFyO" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                            </div>
+                        
+                        {/* added the table of locations of shelters */}
+                        
+                        <div className="tableContainer">
+                            <table {...getTableProps()}>
+                                <thead>
+                                    {headerGroups.map(headerGroup => {
+                                        const {key, ...rest } = headerGroup.getHeaderGroupProps();
+                                            return (
+                                        <tr key={key} {...rest}>
+                                            {headerGroup.headers.map(column => {
+                                                const {key: columnKey, ...columnReset} = column.getHeaderProps();
+                                                return(
+                                                     <th key={columnKey} {...columnReset}>
+                                                    {column.render("Header")}
+                                                </th>
+                                                );
+                                            })}
+                                        </tr>
+                                        );
+                                    })}
+                                </thead>
+                                <tbody {...getTableBodyProps()}>
+                                    {rows.map((row) => {
+                                        prepareRow(row);
+                                            const {key: rowKey, ...rowRest } = row.getRowProps();
+                                            return(
+                                            <tr key={rowKey} {...rowRest}>
+                                                {row.cells.map(cell => {
+                                                    const{key: cellKey, ...cellRest} = cell.getCellProps();
+                                                    return(
+                                                        <td key={cellKey} {...cellRest}>
+                                                            {cell.render("Cell")}
+                                                        </td>
+                                                    );
+                                                })}
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table> 
                         </div>
-                    </div>
-                    <div className="containerFrame"> 
-                    <iframe className="responsiveFrame" width="560" height="315" src="https://www.youtube.com/embed/l3GJqNX5x9M?si=_OYDT6U1ObbHwFyO" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-                    </div>
+                    
+
                 </div>
                 <LinkButton to="/" btnClass="homeBtn" label="Home" imgClass="homeImg" imgSrc="/img/homeButton.png" imgalt="Home Button" />
                 <LinkButton to="/plan" btnClass="planBtn" label="Plan Ahead" imgClass="planImg" imgSrc="/img/contactButton.png" imgalt="Plan Button" />
---
+++ vite-stash-emergency-app/src/components/ContactPg/Shelters.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+    export const minnesota = [
+        {id: 1,
+         state:"MN",
+         address:"204 Elm Avenue Moose Lake, MN 55767",
+         buildingName:"Hope Lutheran Church"}
+    ]
+    export const wisconsin = [
+        {id: 1,
+         state:"WI",
+         address:"5151 S 6th Street Milwaukee, WI 53221",
+         buildingName:"Holler Park Lodge"}
+    ]
+    export const missouri = [
+        {id: 1,
+         state:"MO",
+         address:"1309 N Elizabeth Ave Ferguson, MO 63135",
+         buildingName:"Ferguson Church The Nazarene"},
+        {id: 2,
+         state:"MO",
+         address:"6356 Dr Martin Luther King Dr, St. Louis, MO 63133",
+         buildingName:"Friendly Temple Outreach Center"},
+        {id: 3,
+         state:"MO",
+         address:"1408 N Kingshighway St. Louis, MO 63113",
+         buildingName:"Urban League - Point of Distribution"},
+        {id: 4,
+         state:"MO",
+         address:"4343 W. Florissant Ave St Louis, MO 63115",
+         buildingName:"O'Fallon Park YMCA - Point of Distribution"},
+        {id: 5,
+         state:"MO",
+         address:"4144 Dr. Martin Luther King Dr. St. Louis, MO 63113",
+         buildingName:"4TheVille - Point of Distribution"},
+        {id: 6,
+         state:"MO",
+         address:"2612 Wyoming St St. Louis, MO 63118",
+         buildingName:"Peter & Paul Community Services (Garfield Bldg)"},
+        {id: 7,
+         state:"MO",
+         address:"511 Taylor Street Sikeston, MO 63801",
+         buildingName:"YMCA"}
+
+    ]
+    export const kentucky = [
+        {id: 1,
+         state:"KY",
+         address:"410 Monument Square Jamestown, KY 42629",
+         buildingName:"Russell County Courthouse - Disaster Recovery Center"},
+        {id: 2,
+         state:"KY",
+         address:"200 Jessie Lane Somerset, KY 42501",
+         buildingName:"Life Church Somerset"},
+        {id: 3,
+         state:"KY",
+         address:"360 Parker Rd London, KY 40741",
+         buildingName:"West London Baptist Church"},
+        {id: 4,
+         state:"KY",
+         address:"2330 Keavy Rd London, KY 40744",
+         buildingName:"City of London - Emergency Operations Center"}
+
+    ]
