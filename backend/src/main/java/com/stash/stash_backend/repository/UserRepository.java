9029f0c9898ce6a6d9596109f5d28227176bb600
MedinaSoftic <108442934+MedinaSoftic@users.noreply.github.com>
2025-07-15 16:49:48 -0500

    added userrepository, model completed for now

--- backend/src/main/java/com/stash/stash_backend/repository/UserRepository.java
+++ backend/src/main/java/com/stash/stash_backend/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.stash.stash_backend.repository;
 
-public class UserRepository {
+import com.stash.stash_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
