7fe27d113ac57b03d305d528c13b0ef4d3819d78
MedinaSoftic <108442934+MedinaSoftic@users.noreply.github.com>
2025-07-16 17:18:15 -0500

    added BCrypt as Bean to inject automattically

--- backend/src/main/java/com/stash/stash_backend/SecurityConfig.java
+++ backend/src/main/java/com/stash/stash_backend/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.stash.stash_backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -18,5 +19,10 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 );
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
