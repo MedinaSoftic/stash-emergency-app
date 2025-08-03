c39f4c3a0f60e1fe17bd2a244711e24bc36ca641
MedinaSoftic <108442934+MedinaSoftic@users.noreply.github.com>
2025-08-03 12:16:32 -0500

    added comments to how dashboard protected works

--- vite-stash-emergency-app/src/components/DashboardPg/DashBoardProtected.jsx
+++ vite-stash-emergency-app/src/components/DashboardPg/DashBoardProtected.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../User/UserContext";
 
+//Protects the dashboard route from unauthenticated users 
 export default function DashBoardProtected({ children }) {
   const { user } = useUser();
 
   if (!user) {
     return <Navigate to="/signin" replace />;
   }
-
+// if user is logged in, render to dashboard (the child component)
   return children;
 }
