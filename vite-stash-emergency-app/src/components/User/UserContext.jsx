f7d4da204a3d7678bbd864fe73d12012e951e400
MedinaSoftic <108442934+MedinaSoftic@users.noreply.github.com>
2025-08-04 16:55:56 -0500

    user context was eddited

--- vite-stash-emergency-app/src/components/User/UserContext.jsx
+++ vite-stash-emergency-app/src/components/User/UserContext.jsx
@@ -2,18 +2,21 @@ import { createContext, useState, useContext } from "react";
 
 export const UserContext = createContext();
 
+//Component the wraps around parts of app that need acess to user data
 export function UserProvider({ children }) {
   const [user, setUser] = useState(); // user = { id, name, email }
 
   console.log("UserProvider is active. Current user:", user);
 
   return (
+    //Provide the user state and update function to all children components
     <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
 }
 
+// custom hook allows access to user context
 export function useUser() {
   return useContext(UserContext);
 }
